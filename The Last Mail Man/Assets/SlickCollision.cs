24d0cd8fbfcb36c8819416e2a55c11bc4a23a115
ConnorLiamMurphy <con.el9125@gmail.com>
2026-04-15 19:38:32 -0400

    manual merge because auto merge sucks

---
+++ The Last Mail Man/Assets/SlickCollision.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class SlickCollision : MonoBehaviour
+{
+    private bool isOnOil = false;
+    private bool slow = false;
+    private GameObject enemy;
+    private EnemyAI enemyAI; 
+
+
+    void Start()
+    {
+        enemy = GameObject.Find("Enemy");
+        enemyAI = enemy.GetComponent<EnemyAI>();
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        isOnOil = true;
+
+        if (collider.gameObject.tag == "Player"){
+
+        }
+        if (collider.gameObject.tag == "Enemy")
+        {
+            slow = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        isOnOil = false;
+        slow = false;
+        enemyAI.notSlowed();
+    }
+
+    void Update()
+    {
+        if (isOnOil)
+        {
+            if (slow)
+            {
+                
+                enemyAI.slowed();
+            }
+        }
+    }
+}
